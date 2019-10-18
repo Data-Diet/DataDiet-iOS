b6d7c8ff99d8f848932586946c2d3359f25965bb
Jeremy Manalo <jeremy.h.manalo@gmail.com>
2019-10-18 13:42:08 -0700

    new branch for connect firebase to settings

--- DataDiet/ProductViewController.swift
+++ DataDiet/ProductViewController.swift
@@ -41,41 +41,6 @@ class ProductViewController: UIViewController {
     //this function is fetching the json from URL
     func getJsonFromUrl(){
         //creating a NSURL
-        let url = NSURL(string: URL_HEROES)
-        
-        //fetching the data from the url
-        URLSession.shared.dataTask(with: (url as URL?)!, completionHandler: {(data, response, error) -> Void in
-            
-            if let jsonObj = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? NSDictionary {
-                
-                //printing the json in console
-                print(jsonObj.value(forKey: "avengers")!)
-                
-                //getting the avengers tag array from json and converting it to NSArray
-                if let heroeArray = jsonObj.value(forKey: "avengers") as? NSArray {
-                    //looping through all the elements
-                    for heroe in heroeArray{
-                        
-                        //converting the element to a dictionary
-                        if let heroeDict = heroe as? NSDictionary {
-                            
-                            //getting the name from the dictionary
-                            if let name = heroeDict.value(forKey: "name") {
-                                
-                                //adding the name to the array
-                                self.nameArray.append((name as? String)!)
-                            }
-                            
-                        }
-                    }
-                }
-                
-                OperationQueue.main.addOperation({
-                    //calling another function after fetching the json
-                    //it will show the names to label
-                })
-            }
-        }).resume()
     }
     
     
