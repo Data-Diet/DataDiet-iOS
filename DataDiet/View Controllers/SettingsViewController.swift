a59eecf46a505580bca0cbb9551482e50c8e7072
Jeremy Manalo <jeremy.h.manalo@gmail.com>
2019-11-18 00:28:48 -0800

    Adding all files

---
+++ DataDiet/View Controllers/SettingsViewController.swift
@@ -0,0 +1,54 @@
+//
+//  SettingsViewController.swift
+//  DataDiet
+//
+//  Created by Eric Zamora on 11/7/19.
+//  Copyright © 2019 DataDiet. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+import Firebase
+import GoogleSignIn
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var logOutButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    
+    
+    @IBAction func logOutTapped(_ sender: Any) {
+        
+        GIDSignIn.sharedInstance().signOut()
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+            //DispatchQueue.main.async {
+                //self.performSegue(withIdentifier: "loggedOut", sender: self)}
+            //navigationController?.popToRootViewController(animated: true)
+            //let navController = UINavigationController(rootViewController: //LoginViewController())
+            //navController.navigationBar.barStyle = .black
+            //self.present(navController, animated: true, completion: nil)
+            performSegue(withIdentifier: "logOUT", sender: sender)
+            print("Logged OUT")
+        }catch let signOutError as NSError{
+                print("Error signing out: %@", signOutError)
+            }
+    }
+}
---
+++ DataDiet/View Controllers/USDA_API.swift
@@ -0,0 +1,113 @@
+//
+//  USDA_API.swift
+//  USDA_API
+//
+//  Created by Kenneth Mai on 10/29/19.
+//  Copyright © 2019 Kenneth Mai. All rights reserved.
+//
+
+/* How to make a USDA request for ingredients ****************************
+
+ Input: Barcode Number, Output: Ingredients Array
+ 
+ Examaple Call:
+ 
+ let data = USDARequest()
+ data.getIngredients(barcodeNumber: "00028400165440") { (ingredientsArray) in
+     //Can access all the ingredients in here if barcode is specified
+     for element in ingredientsArray { //Testing
+         print(element)
+     }
+ }
+ 
+**************************************************************************/
+
+import Foundation
+
+struct USDARequest{
+     
+    let API_KEY = "aGdpm2mtlYcffnTgvTbh8axBPC9wdn8Z4tF0HvJ5"
+    
+    func containsFoodProduct(barcodeNumber: String, completionHandler: @escaping ((Array<String>) -> Bool)) {
+        var params:[String: Any]?
+        let session = URLSession.shared
+        let url = "https://api.nal.usda.gov/fdc/v1/search?api_key=\(API_KEY)"
+        let request = NSMutableURLRequest(url: NSURL(string: url)! as URL)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        params = ["generalSearchInput":"\(barcodeNumber)"]
+ 
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: params, options: JSONSerialization.WritingOptions())
+            let task = session.dataTask(with: request as URLRequest as URLRequest, completionHandler: { (data, response, error) in
+               guard let data = data else { return }
+               
+               if let ingredientsString = String(data: data, encoding: .utf8) {
+                   let ingredients = self.formatIngredients(ingredientsList: ingredientsString)
+                   completionHandler(ingredients)
+               }
+               
+            } )
+            task.resume()
+        } catch {
+            print ("Error")
+        }
+    }
+    
+    func getIngredients(barcodeNumber: String, completionHandler: @escaping ((Array<String>) -> Void)) {
+         var params:[String: Any]?
+         let session = URLSession.shared
+         let url = "https://api.nal.usda.gov/fdc/v1/search?api_key=\(API_KEY)"
+         let request = NSMutableURLRequest(url: NSURL(string: url)! as URL)
+         request.httpMethod = "POST"
+         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+         params = ["generalSearchInput":"\(barcodeNumber)"]
+        
+         do {
+             request.httpBody = try JSONSerialization.data(withJSONObject: params, options: JSONSerialization.WritingOptions())
+            
+             let task = session.dataTask(with: request as URLRequest as URLRequest, completionHandler: { (data, response, error) in
+                guard let data = data else { return }
+                
+                if let ingredientsString = String(data: data, encoding: .utf8) {
+                    let ingredients = self.formatIngredients(ingredientsList: ingredientsString)
+                    completionHandler(ingredients)
+    
+                    print()
+                    print("USDA product response:")
+                    print(ingredientsString)
+                    print()
+                }
+             } )
+             task.resume()
+         } catch {
+             print ("Error")
+         }
+     }
+    
+    //Function used to format ingredients list into an array
+    func formatIngredients(ingredientsList: String) -> Array<String> {
+        let ingredients = ingredientsList.substring(from: "ingredients" + "\"" + ":" + "\"", to: ".");
+        
+        guard var rmChars = ingredients?.replacingOccurrences(of: "[\\[\\]\\(\\)]", with: "", options: .regularExpression, range: nil) else {
+                print("Barcode was not found in USDA Database")
+                return [String]()
+            }
+        
+        rmChars = rmChars.replacingOccurrences(of: "Made from ", with: "")
+        rmChars = rmChars.replacingOccurrences(of: "and ", with: "")
+        rmChars = rmChars.replacingOccurrences(of: "and/or ", with: "")
+        return rmChars.components(separatedBy: ", ")
+    }
+    
+}
+
+//Function used for getting ingrdients list from JSON data
+extension StringProtocol  {
+    func substring(from start: Self, to end: Self? = nil, options: String.CompareOptions = []) -> SubSequence? {
+        guard let lower = range(of: start, options: options)?.upperBound else { return nil }
+        guard let end = end else { return self[lower...] }
+        guard let upper = self[lower...].range(of: end, options: options)?.lowerBound else { return nil }
+        return self[lower..<upper]
+    }
+}
