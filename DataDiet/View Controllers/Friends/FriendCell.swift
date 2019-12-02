5ce032f881656f0a6fb0e5e38754a310d9cb2c30
Jeremy Manalo <jeremy.h.manalo@gmail.com>
2019-12-02 13:41:20 -0800

    adding friend cell

--- DataDiet/View Controllers/Friends/FriendCell.swift
+++ DataDiet/View Controllers/Friends/FriendCell.swift
@@ -13,18 +13,8 @@ class FriendCell: UITableViewCell {
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var fullNameLabel: UILabel!
     @IBOutlet weak var usernameLabel: UILabel!
-    /*override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }*/
     
-    func setUser(user: User){
+    func setUser(user: User) {
         profileImageView.image = user.image
         profileImageView.layer.masksToBounds = true
         profileImageView.layer.cornerRadius = profileImageView.bounds.width / 2
